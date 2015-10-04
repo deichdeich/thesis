b298b005b07829893cdaaa8dd599f13fd3d6ef9d
deichdeich <xdeich@gmail.com>
2015-10-04 13:09:41 -0700

    for playing around with precession & odeint

--- code/mine/simpleode.py
+++ code/mine/simpleode.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.integrate import odeint
 
-init_state = [.9,0]
+init_state = [.7,0]
 
 def dstate_dt(state,t):
     x = state[0]
@@ -11,15 +11,14 @@ def dstate_dt(state,t):
     dstate = np.zeros_like(state)
     
     dstate[0] = xdot
-    #dstate[1] = -2 * x**3 * (1 - x * xdot - xdot) + (x**2 * xdot - x**3 * xdot)
     
     # simple precession test case
     # along with this x'', make init_state = [<1,~0]
-    # dstate[1] = -x*(.5) + .5
+    dstate[1] = -x*(.5) + .5
     
     return dstate
     
-t = np.arange(0.0,15*np.pi,0.001)
+t = np.arange(0.0,20*np.pi,0.001)
 
 finalstate = odeint(dstate_dt,init_state,t)
 

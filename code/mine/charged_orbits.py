2c9d321ad7b28598a973ae3bbd1ca6618ac8ca0c
deichdeich <xdeich@gmail.com>
2015-10-14 23:54:12 -0700

    it works!

--- code/mine/charged_orbits.py
+++ code/mine/charged_orbits.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 
 # [r,rdot,phi,phidot] 
-init_state = [.02,88.,0.001,-90.]
+init_state = [2.,0.,0.,0.]
 
 
 # define some constants
@@ -16,21 +16,24 @@ Q = 1.
         
 def dstate_dt(state, t): 
     # unpack state vector
-    L = 1.
-    r = state[0]+0.000001
+    
+    if t<100:
+        L = -0.005
+    if t>100:
+        L = 0.005
+    
+    #L = -0.005
+    r = state[0]
     rdot = state[1]
     phi = state[2]
-    phidot = state[3]
         
     dstate = np.zeros_like(state)
     dstate[0] = rdot
-    dstate[1] = ((r * phidot**2) - 1/(r**2) + (L * phidot)/(r**2))
-    dstate[2] = phidot
-    dstate[3] = -(L/(r**2) - rdot/r)
-        
+    dstate[1] = 1/r**3 - 1/r**2 + L * (-1/r**4 - L/(r**5))
+    dstate[2] = 1/(r**2) + L/r**4
     return dstate
     
-t = np.arange(0.1,1.,0.00001)
+t = np.arange(0.1,200.,0.001)
     
 finalstate = odeint(dstate_dt, init_state, t)
     
@@ -45,11 +48,11 @@ y = r * np.sin(phi)
     
     
 plt.figure()
-plt.scatter(x,y,c=np.log10(t),s=10,edgecolors='none')
+plt.scatter(x,y,c=(t),s=10,edgecolors='none')
 plt.xlabel('X position')
 plt.ylabel('Y position')
 cb = plt.colorbar()
-cb.set_label('log(time)')
+cb.set_label('time')
 plt.scatter(0,0,marker='x',s=100,color='black')
 plt.show()
 """

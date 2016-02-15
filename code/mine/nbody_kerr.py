69dab665bb45b173692b442acb659ad20fa239a2
deichdeich <xdeich@gmail.com>
2016-02-14 20:11:44 -0800

    Python port

--- code/mine/nbody_kerr.py
+++ code/mine/nbody_kerr.py
@@ -1,7 +1,7 @@
 from __future__ import division, print_function
 import numpy as np
 import time
-import curses
+import sys
 import ThesisTools as farts
 from astropy.io import fits
 
@@ -9,12 +9,12 @@ class OrbitalSystem(object):
     def __init__(self,
                  Nparticles,
                  M,
-                 a,
+                 a = None,
                  dt=0.001,
                  interaction_type = "ClassicalNBody",
                  masses = None,
                  use_state = None,
-                 save_dir = "Users/alexdeich/Dropbox/thesis/code/nbody_output"):
+                 save_dir = "/Users/alexdeich/Dropbox/thesis/code/mine/nbody_output"):
         
         self.start_particles = Nparticles
         self.Nparticles = Nparticles
@@ -24,6 +24,7 @@ class OrbitalSystem(object):
         self.a = a
         self.event_horizon = 0
         self.cleanup = []
+        self.use_state = use_state
         self.save_dir = save_dir
         
         if use_state is not None:
@@ -33,7 +34,7 @@ class OrbitalSystem(object):
                 raise ValueError("Initial state not the right shape: ",use_state.shape,
                                  "\nShould be ",(Nparticles,2,2))
     
-        if masses == None,
+        if masses == None:
             self.masses = np.zeros(Nparticles)+0.0001
         else:
             if len(masses) == Nparticles:
@@ -44,9 +45,10 @@ class OrbitalSystem(object):
     
     def SingleParticleDerivativeVector(self,kstate,particle,t):
         if self.interaction_type == "ClassicalNBody":
-            rad = farts.xy2rad(state[particle],
+            rad = farts.xy2rad(self.state[particle],
                                farts.SingleParticleNewtonianForce(kstate,
                                                                   self.masses,
+                                                                  particle,
                                                                   self.Nparticles,
                                                                   2))
                                                                   
@@ -60,20 +62,19 @@ class OrbitalSystem(object):
         
         r = rad[0,0]
         
-        if(r > 999 or r < self.event_horizon)):
+        if(r > 999 or r < self.event_horizon):
             self.cleanup.append(particle)
            
         phi = rad[0,1]
         
         f = np.array((rad[0],rad[1]))
+        G = np.array([[f[0,1],
+                       -(1/f[0,0]**4)*(self.a(t)**2 - 2*self.M*f[0,0] + (f[0,0]**2))*((self.M*(f[0,0]**4)*f[0,1]**2)/(self.a(t)**2 - 2*self.M*f[0,0] + (f[0,0]**2))**2 - ((f[0,0]**5)*(f[0,1]**2))/((self.a(t)**2) - 2*self.M*f[0,0] + (f[0,0]**2))**2 + self.M*(-1 + self.a(t)*f[1,1])**2 +  (f[0,0]**3)*((f[0,1]**2)/((self.a(t)**2) - 2*self.M*f[0,0] + (f[0,0]**2)) - (f[1,1]**2))) + rad[2, 0]],
+                       [f[1,1],
+                       -((2*f[0,1]*(self.a(t)*self.M + (-(self.a(t)**2)*self.M + f[0,0]**3)*f[1,1]))/(f[0,0]*(2*(self.a(t)**2)*self.M + (self.a(t)**2)*f[0,0] + (f[0,0]**3)))) + rad[2, 1]]])
         
-        G = np.array([[f[1],
-                       -(1/f[0]**4)*(self.a(t)**2 - 2*self.M*f[0] + (f[0]**2))*((self.M*(f[0]**4)*f[1]**2)/(self.a(t)**2 - 2*self.M*f[0] + (f[0]**2))**2 - ((f[0]**5)*(f[1]**2))/((self.a(t)**2) - 2*self.M*f[0] + (f[0]**2))**2 + self.M*(-1 + self.a(t)*f[3])**2 +  (f[0]**3)*((f[1]**2)/((self.a(t)**2) - 2*self.M*f[0] + (f[0]**2)) - (f[3]**2))) + rself.ad[2, 0]],
-                       [f[3],
-                       -((2*f[1]*(self.a(t)*self.M + (-(self.a(t)**2)*self.M + f[0]**3)*f[3]))/(f[0]*(2*(self.a(t)**2)*self.M + (self.a(t)**2)*f[0] + (f[0]**3)))) + rself.ad[2, 1]]])
         
-        
-        return(G2xy(G,r,phi))
+        return(farts.G2xy(G,r,phi))
         
     def UpdateStateVectorRK4(self,t):
         self.event_horizon = self.M + np.sqrt(self.M**2 - self.a(t)**2)
@@ -82,13 +83,13 @@ class OrbitalSystem(object):
         for particle in xrange(self.Nparticles):
             kstate = self.state
             #do RK4 shit
-            k1 = dt * SingleParticleDerivativeVector(kstate,particle, t+self.dt)
+            k1 = self.dt * self.SingleParticleDerivativeVector(kstate,particle, t+self.dt)
             kstate[particle] = self.state[particle] + k1/2
-            k2 = dt * SingleParticleDerivativeVector(kstate,particle,t+(self.dt/2))
+            k2 = self.dt * self.SingleParticleDerivativeVector(kstate,particle,t+(self.dt/2))
             kstate[particle] = self.state[particle] + k2/2
-            k3 = dt * SingleParticleDerivativeVector(kstate,particle,t+(self.dt/2))
+            k3 = self.dt * self.SingleParticleDerivativeVector(kstate,particle,t+(self.dt/2))
             kstate[particle] = self.state[particle] + k3
-            k4 = dt * SingleParticleDerivativeVector(kstate,particle,t+self.dt)
+            k4 = self.dt * self.SingleParticleDerivativeVector(kstate,particle,t+self.dt)
             new_state[particle] = self.state[particle] + (1/3)*(k1/2 + k2 + k3 + k4/2)
             
         #Get rid of gobbled or ejected particles 
@@ -112,39 +113,47 @@ class OrbitalSystem(object):
     def TimeEvolve(self,nsteps,comments):
         t=0
         ##Get init_state
-        if use_state == None:
-            self.state = MakeInitialConditions()
+        if self.use_state == None:
+            self.state = self.MakeInitialConditions()
         
         print("Generated initial conditions")
         
         
-        primary = get_header()
-        frame0 = get_hdu()
+        primary = self.get_header(nsteps,comments)
+        frame0 = self.get_hdu()
         
         hdulist = fits.HDUList([primary,frame0])
+        total_time = 0
         for step in xrange(1, nsteps):
             stepstart = time.time()
-            self.state = UpdateStateVectorsRK4(t)
-            framen = get_hdu()
+            self.state = self.UpdateStateVectorRK4(t)
+            framen = self.get_hdu()
             hdulist.append(framen)
             t += self.dt
             end = time.time()
-            sys.stdout.write("\rFrame {} of {} completed ({}%).  Last frame took {} seconds.".format(step,
+            steptime = end-stepstart
+            total_time += steptime
+            sys.stdout.write("\rFrame {} of {} completed ({}%).  Last frame took {} seconds, {} seconds total".format(step+1,
                                                                                                     nsteps,
-                                                                                                    (step/nsteps)*100),
-                                                                                                    end-start)
+                                                                                                    ((step+1)/nsteps)*100,
+                                                                                                    steptime,
+                                                                                                    total_time))
         
-        filenum = farts.get_filenum(dir)
-        fname = "{}/nbody_{}_{}.fits".format(dir,self.start_particles,filenum)
+        filenum = farts.get_filenum(self.save_dir)
+        fname = "{}/nbody_{}_{}.fits".format(self.save_dir,self.start_particles,filenum)
         hdulist.writeto(fname,clobber=True)
     
-    def get_header(self):
+    def get_header(self,nsteps,comments=""):
+        
         prihdr = fits.Header()
-        prihdr["NPARTICLES"] = self.Nparticles
-        prihdr["INTERACTION"] = self.interaction_type
+        prihdr["NPARTS"] = self.Nparticles
+        prihdr["INTERACT"] = self.interaction_type
         prihdr["DT"] = self.dt
         prihdr["BH_M"] = self.M
-        prihdr["SPIN_FUNC"] = self.a
+        if self.a:
+            prihdr["SPINFUNC"] = True
+        else:
+            prihdr["SPINFUNC"] = False
         prihdr["NSTEPS"] = nsteps
         prihdr["COMMENTS"] = comments
         prihdu = fits.PrimaryHDU(header=prihdr)
@@ -152,7 +161,7 @@ class OrbitalSystem(object):
         return(prihdu)
         
     def get_hdu(self):
-        transpose_state = self.state.T
+        state_transpose = self.state.T
         frame = fits.BinTableHDU.from_columns([fits.Column(name='X',format='20A',array = state_transpose[0][0]),
                                                 fits.Column(name='Y',format='20A',array = state_transpose[1][0]),
                                                 fits.Column(name='Xd',format='20A',array = state_transpose[0][1]),
@@ -164,7 +173,7 @@ class OrbitalSystem(object):
         self.state = np.delete(self.state,particle,axis=0)
         self.Nparticles -= 1
     
-    def combine_particles(self,particle1,particle2)
+    def combine_particles(self,particle1,particle2):
         #get phase space of new particle
         
         #delete collided particles in state and mass
@@ -196,9 +205,12 @@ class OrbitalSystem(object):
         self.Nparticles -= 1
         
     def trajplot(self,interval):
+        raise ValueError("This is not implemented yet")
     
     def densityplot(self,frame):
+        raise ValueError("This is not implemented yet")
     
     def stats(self, interval, statistical_function):
         #If len(interval)==1: return histogram of that frame
         #Else: return plot of statistical_function over interval
+        raise ValueError("This is not implemented yet")

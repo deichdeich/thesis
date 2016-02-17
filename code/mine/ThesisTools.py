6cbed46f8e849d6da1843549ba9a65d5fd13113e
deichdeich <xdeich@gmail.com>
2016-02-16 17:33:57 -0800

    got everything working for 1-particle cases

--- code/mine/ThesisTools.py
+++ code/mine/ThesisTools.py
@@ -3,8 +3,8 @@ import os
 import re
 import numpy as np
 
-def SingleParticleNewtonianForce(state, masses, i, Nparticles, eps):
-    forces = np.empty([Nparticles,2])
+def SingleParticleNewtonianForce(state, masses, i, Nparticles, soi_radius):#, collision_radius):
+    forces = np.zeros([Nparticles,2])
     
     x1 = state[i,0,0]
     y1 = state[i,0,1]
@@ -15,15 +15,16 @@ def SingleParticleNewtonianForce(state, masses, i, Nparticles, eps):
             x2 = state[particle_num,0,0]
             y2 = state[particle_num,0,1]
             distance2 = ((x2-x1)**2+(y2-y1)**2)
-            if distance2 < eps:
+            if distance2 < soi_radius:
                 jforce = m2/distance2
                 jforcedir = [x2-x1,y2-y1]/np.sqrt(distance2)
                 forces[particle_num] = jforce*jforcedir
+            
     return(np.sum(forces,axis=0))
 
 def G2xy(G,r,T):
     
-    returnG = np.empty(G.shape)
+    returnG = np.zeros_like(G)
     
     rd = G[0,0]
     rdd = G[0,1]
@@ -31,9 +32,9 @@ def G2xy(G,r,T):
     Tdd = G[1,1]
     
     xd = np.cos(T)*rd-r*np.sin(T)*Td
-    xdd = np.cos(T)*(-r*Td**2+rdd)-np.sin(T)*(2*rd*Td+r*Tdd)
+    xdd = np.cos(T)*(-r*(Td**2)+rdd)-np.sin(T)*(2*rd*Td+r*Tdd)
     yd = np.sin(T)*rd+np.cos(T)*r*Td
-    ydd = 2*np.cos(T)*rd*Td+np.sin(T)*(-r*Td**2+rdd)+np.cos(T)*r*Tdd
+    ydd = 2*np.cos(T)*rd*Td+np.sin(T)*(-r*(Td**2)+rdd)+np.cos(T)*r*Tdd
     
     returnG[0] = [xd,yd]
     returnG[1] = [xdd,ydd]
@@ -43,18 +44,19 @@ def G2xy(G,r,T):
 def get_filenum(dir):
     filenums = [[0]]
     
-    regex = re.compile(r'\d+.csv')
+    regex = re.compile(r'\d+')
     
     for filename in os.listdir(dir):
-        if filename[:5] == "nbody":
-            filenums.append([int(x) for x in regex.findall(filename)])
+        
+        if(filename[:5] == "nbody" and filename[-5:] == ".fits"):
+            filenums.append([int(x) for x in regex.findall(filename[-7:])])
     return(1+int(max(filenums)[0]))
 
 
 
 def xy2rad(state,forces):
     
-    radstate = np.empty([3,2])
+    radstate = np.zeros([3,2])
 
     x = state[0,0]
     y = state[0,1]
@@ -67,7 +69,7 @@ def xy2rad(state,forces):
     r2 = x**2+y**2
     r = np.sqrt(r2)
     if x>0:
-        T = np.arctan(y/x)+np.pi
+        T = np.arctan(y/x)
     elif x<0:
         if y>=0:
             T = np.arctan(y/x)+np.pi
@@ -81,20 +83,22 @@ def xy2rad(state,forces):
         elif y==0:
             T = 0
     else:
-        print x,y
+        print state
         raise ValueError("NaN encountered")
     
     #velocities
     rd = (x*xd+y*yd)/r
-    Td = (x*xd-y*yd)/r2
+    Td = (x*yd-y*xd)/r2
     
     #Accelerations
-    rdd = (-4*(x*xd+y*yd)**2+4*r2*(xd**2+yd**2+x*xdd+y*ydd))/(4*(r2)**(3/2));
-    Tdd = (y**2*(-xdd*y+2*xd*yd)-x**2*(xdd*y+2*xd*yd)+x**3*ydd+x*y*(2*xd**2-2*yd**2+y*ydd))/(r2**2)
-    
+    rdd = (-4*(x*xd+y*yd)**2+4*r2*(xd**2+yd**2+x*xdd+y*ydd))/(4*(r2**(3/2)));
+    Tdd = (y**2*(-xdd*y+2*xd*yd)-((x**2)*(xdd*y+2*xd*yd))+(x**3)*ydd+x*y*(2*(xd**2)-2*(yd**2)+y*ydd))/(r2**2)
     
     radstate[0] = [r,T]
     radstate[1] = [rd,Td]
-    radstate[2] = [rdd-r*Td**2,r*Tdd+2*rd*Td]
+    radstate[2] = [rdd-r*(Td**2),r*Tdd+2*rd*Td]
     
     return(radstate)
+
+def a0(x):
+        return(0)
--- code/mine/nbody_kerr.py
+++ code/mine/nbody_kerr.py
@@ -1,5 +1,5 @@
 from __future__ import division, print_function
-import numpy as np
+import numpy128 as np
 import time
 import sys
 import ThesisTools as farts
@@ -9,7 +9,7 @@ class OrbitalSystem(object):
     def __init__(self,
                  Nparticles,
                  M,
-                 a = None,
+                 a = farts.a0,
                  dt=0.001,
                  interaction_type = "ClassicalNBody",
                  masses = None,
@@ -24,8 +24,11 @@ class OrbitalSystem(object):
         self.a = a
         self.event_horizon = 0
         self.cleanup = []
-        self.use_state = use_state
+        self.use_state = np.copy(use_state)
         self.save_dir = save_dir
+        self.init_state = np.copy(use_state)
+        self.state = np.copy(use_state)
+        self.fname = ""
         
         if use_state is not None:
             if use_state.shape == (Nparticles,2,2):
@@ -45,7 +48,7 @@ class OrbitalSystem(object):
     
     def SingleParticleDerivativeVector(self,kstate,particle,t):
         if self.interaction_type == "ClassicalNBody":
-            rad = farts.xy2rad(self.state[particle],
+            rad = farts.xy2rad(kstate[particle],
                                farts.SingleParticleNewtonianForce(kstate,
                                                                   self.masses,
                                                                   particle,
@@ -59,49 +62,54 @@ class OrbitalSystem(object):
             raise ValueError("ClassicalElastic collisions not implemented yet")
         
         
-        
-        r = rad[0,0]
-        
-        if(r > 999 or r < self.event_horizon):
-            self.cleanup.append(particle)
-           
+        r = rad[0,0]   
         phi = rad[0,1]
         
-        f = np.array((rad[0],rad[1]))
-        G = np.array([[f[0,1],
-                       -(1/f[0,0]**4)*(self.a(t)**2 - 2*self.M*f[0,0] + (f[0,0]**2))*((self.M*(f[0,0]**4)*f[0,1]**2)/(self.a(t)**2 - 2*self.M*f[0,0] + (f[0,0]**2))**2 - ((f[0,0]**5)*(f[0,1]**2))/((self.a(t)**2) - 2*self.M*f[0,0] + (f[0,0]**2))**2 + self.M*(-1 + self.a(t)*f[1,1])**2 +  (f[0,0]**3)*((f[0,1]**2)/((self.a(t)**2) - 2*self.M*f[0,0] + (f[0,0]**2)) - (f[1,1]**2))) + rad[2, 0]],
-                       [f[1,1],
-                       -((2*f[0,1]*(self.a(t)*self.M + (-(self.a(t)**2)*self.M + f[0,0]**3)*f[1,1]))/(f[0,0]*(2*(self.a(t)**2)*self.M + (self.a(t)**2)*f[0,0] + (f[0,0]**3)))) + rad[2, 1]]])
+        if(r > 999 or r < self.event_horizon):
+            if particle not in self.cleanup:
+                self.cleanup.append(particle)
         
+        f = np.array(([rad[0,0],rad[1,0]],[rad[0,1],rad[1,1]]))
+        G=np.array([[f[0,1],
+                     -(1/f[0,0]**4)*(self.a(t)**2-2*self.M*f[0,0]+(f[0,0]**2))*((self.M*(f[0,0]**4)*f[0,1]**2)/(self.a(t)**2-2*self.M*f[0,0]+(f[0,0]**2))**2-((f[0,0]**5)*(f[0,1]**2))/((self.a(t)**2)-2*self.M*f[0,0]+(f[0,0]**2))**2+self.M*(-1+self.a(t)*f[1,1])**2+(f[0,0]**3)*((f[0,1]**2)/((self.a(t)**2)-2*self.M*f[0,0]+(f[0,0]**2))-(f[1,1]**2)))+rad[2,0]],
+                     [f[1,1],
+                     -((2*f[0,1]*(self.a(t)*self.M+(-(self.a(t)**2)*self.M+f[0,0]**3)*f[1,1]))/(f[0,0]*(2*(self.a(t)**2)*self.M+(self.a(t)**2)*f[0,0]+(f[0,0]**3))))+rad[2,1]]])
         
         return(farts.G2xy(G,r,phi))
         
     def UpdateStateVectorRK4(self,t):
-        self.event_horizon = self.M + np.sqrt(self.M**2 - self.a(t)**2)
+        self.event_horizon = self.get_event_horizon(t)
         new_state = np.ndarray(self.state.shape)
         
         for particle in xrange(self.Nparticles):
-            kstate = self.state
+            kstate = np.copy(self.state)
             #do RK4 shit
             k1 = self.dt * self.SingleParticleDerivativeVector(kstate,particle, t+self.dt)
-            kstate[particle] = self.state[particle] + k1/2
+            kstate[particle] = np.copy(self.state[particle]) + k1/2
             k2 = self.dt * self.SingleParticleDerivativeVector(kstate,particle,t+(self.dt/2))
-            kstate[particle] = self.state[particle] + k2/2
+            kstate[particle] = np.copy(self.state[particle]) + k2/2
             k3 = self.dt * self.SingleParticleDerivativeVector(kstate,particle,t+(self.dt/2))
-            kstate[particle] = self.state[particle] + k3
+            kstate[particle] = np.copy(self.state[particle]) + k3
             k4 = self.dt * self.SingleParticleDerivativeVector(kstate,particle,t+self.dt)
-            new_state[particle] = self.state[particle] + (1/3)*(k1/2 + k2 + k3 + k4/2)
+            new_state[particle] = np.copy(self.state[particle]) + (1/3)*(k1/2 + k2 + k3 + k4/2)
             
         #Get rid of gobbled or ejected particles 
-        for particle in self.cleanup:
-            self.remove_particle(particle) 
+        if self.cleanup != []:
+            for particle in self.cleanup:
+                print("\n***particle {} shit the bed at step {}***".format(particle,int(t/self.dt)))
+                self.remove_particle(particle)
+                print("***particle {} removed***".format(particle))
+                self.cleanup.remove(particle)
+                if self.cleanup == []:
+                    print("***cleanup completed***")
         self.cleanup = []
         
         return(new_state)
     
     def MakeInitialConditions(self):
-        vecs = np.ndarray((self.Nparticles,2,2))
-        for vec in xrange(self.Nparticles):
+        self.cleanup = []
+        vecs = np.zeros((self.start_particles,2,2))
+        for vec in xrange(self.start_particles):
             r = ((10-5)*np.random.random())+5
             phi = 2*np.pi*np.random.random()
             phid = ((1.5-0.5)*np.random.random())+0.5
@@ -111,13 +119,16 @@ class OrbitalSystem(object):
         
         
     def TimeEvolve(self,nsteps,comments):
+        self.cleanup = []
         t=0
         ##Get init_state
         if self.use_state == None:
             self.state = self.MakeInitialConditions()
-        
-        print("Generated initial conditions")
-        
+            self.init_state = self.state
+        else:
+            self.state = np.copy(self.use_state)
+        print("Got initial conditions for {} particles".format(self.start_particles))
+
         
         primary = self.get_header(nsteps,comments)
         frame0 = self.get_hdu()
@@ -133,15 +144,20 @@ class OrbitalSystem(object):
             end = time.time()
             steptime = end-stepstart
             total_time += steptime
-            sys.stdout.write("\rFrame {} of {} completed ({}%).  Last frame took {} seconds, {} seconds total".format(step+1,
-                                                                                                    nsteps,
-                                                                                                    ((step+1)/nsteps)*100,
-                                                                                                    steptime,
-                                                                                                    total_time))
-        
+            avg = total_time/step
+            perc = 100*((step+1)/nsteps)
+            sys.stdout.write('\rFrame {} of {} completed ({}%).  Step: {}s, Total: {}s, Estimated time remaining: {}s.'.format(step+1,
+                                                                       nsteps,
+                                                                       '%0.1f'%perc,
+                                                                       '%0.4f'%steptime,
+                                                                       '%0.4f'%total_time,
+                                                                       '%i'%(((avg * nsteps)+1)-total_time)))
+            sys.stdout.flush()
+        print("\nWriting to disk...")
         filenum = farts.get_filenum(self.save_dir)
-        fname = "{}/nbody_{}_{}.fits".format(self.save_dir,self.start_particles,filenum)
-        hdulist.writeto(fname,clobber=True)
+        self.fname = "{}/nbody_{}_{}.fits".format(self.save_dir,self.start_particles,filenum)
+        hdulist.writeto(self.fname,clobber=True)
+        print("Data written at {}".format(self.fname))
     
     def get_header(self,nsteps,comments=""):
         
@@ -203,9 +219,39 @@ class OrbitalSystem(object):
         self.masses = np.append(self.masses,totalmass,axis=0)
         
         self.Nparticles -= 1
+    
+    def trajplot(self,interval = "all",saveplot = False):
+        framelist = fits.open(self.fname)
         
-    def trajplot(self,interval):
-        raise ValueError("This is not implemented yet")
+        if type(interval) == int:
+            interval = [interval,interval+1]
+        elif interval == "all":
+            interval = [1,len(framelist)]
+        x = []
+        y = []
+        rng = max(interval)-min(interval)
+        if rng > 300:
+            stepsize = 1
+        elif rng < 20000:
+            stepsize = 10
+        else:
+            stepsize = 500
+        for frame in xrange(interval[0],interval[1],stepsize):
+            x.append(float(framelist[frame].data[0][0]))
+            y.append(float(framelist[frame].data[0][1]))
+        
+        import matplotlib.pyplot as plt
+        plt.scatter(x,y, color="black",edgecolors= "None")
+        plt.scatter(0,0,marker='x', color="black")
+        circle1 = plt.Circle((0,0),radius = 2,color='r',fill=False)
+        plt.xlim(-12,12)
+        plt.ylim(-12,12)
+        fig = plt.gcf()
+        fig.gca().add_artist(circle1)
+        plt.show()
+    
+    def get_event_horizon(self,t):
+        return(self.M + np.sqrt(self.M**2 - self.a(t)**2))
     
     def densityplot(self,frame):
         raise ValueError("This is not implemented yet")

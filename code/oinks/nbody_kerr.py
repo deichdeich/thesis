7d0f9144238c542dbae3151b0233bf0eaa88c234
deichdeich <xdeich@gmail.com>
2016-03-05 20:36:37 -0800

    everything works

--- code/oinks/nbody_kerr.py
+++ code/oinks/nbody_kerr.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 import sys
 import os
 import ThesisTools as farts
+import scipy.stats
 from astropy.io import fits
 
 class OrbitalSystem(object):
@@ -47,7 +48,39 @@ class OrbitalSystem(object):
         self.fname_list = []
         self.dirname = ""
         self.plotdata = 0
+        self.skip_mkdir=False
+        self.nsteps = 0
         
+        self.pathlist1 = ['/Users/alexdeich/output/nbody_3000_1/data/1000.fits',
+        '/Users/alexdeich/output/nbody_3000_1/data/2000.fits',
+ 		'/Users/alexdeich/output/nbody_3000_1/data/3000.fits',
+		'/Users/alexdeich/output/nbody_3000_1/data/4000.fits',
+ 		'/Users/alexdeich/output/nbody_3000_1/data/5000.fits',
+ 		'/Users/alexdeich/output/nbody_3000_1/data/6000.fits',
+ 		'/Users/alexdeich/output/nbody_3000_1/data/7000.fits',
+ 		'/Users/alexdeich/output/nbody_3000_1/data/8000.fits',
+ 		'/Users/alexdeich/output/nbody_3000_1/data/9000.fits'
+ 		'/Users/alexdeich/output/nbody_3000_1/data/10000.fits',
+		'/Users/alexdeich/output/nbody_3000_1/data/11000.fits',
+ 		'/Users/alexdeich/output/nbody_3000_1/data/12000.fits',
+ 		'/Users/alexdeich/output/nbody_3000_1/data/13000.fits',
+ 		'/Users/alexdeich/output/nbody_3000_1/data/14000.fits',
+ 		'/Users/alexdeich/output/nbody_3000_1/data/15000.fits',
+ 		'/Users/alexdeich/output/nbody_3000_1/data/16000.fits',
+ 		'/Users/alexdeich/output/nbody_3000_1/data/17000.fits',
+ 		'/Users/alexdeich/output/nbody_3000_1/data/18000.fits',
+ 		'/Users/alexdeich/output/nbody_3000_1/data/19000.fits',
+ 		'/Users/alexdeich/output/nbody_3000_1/data/20000.fits',
+ 		'/Users/alexdeich/output/nbody_3000_1/data/21000.fits',
+ 		'/Users/alexdeich/output/nbody_3000_1/data/22000.fits',
+ 		'/Users/alexdeich/output/nbody_3000_1/data/23000.fits',
+ 		'/Users/alexdeich/output/nbody_3000_1/data/24000.fits',
+ 		'/Users/alexdeich/output/nbody_3000_1/data/25000.fits',
+ 		'/Users/alexdeich/output/nbody_3000_1/data/26000.fits',
+ 		'/Users/alexdeich/output/nbody_3000_1/data/27000.fits',
+ 		'/Users/alexdeich/output/nbody_3000_1/data/28000.fits',
+ 		'/Users/alexdeich/output/nbody_3000_1/data/29000.fits',
+        '/Users/alexdeich/output/nbody_3000_1/data/29999.fits']
         
         if self.Nparticles == None and self.init_state is not None:
             self.Nparticles = len(self.init_state)
@@ -76,7 +109,7 @@ class OrbitalSystem(object):
             rad = farts.xy2rad(kstate[particle],
                                self.SingleParticleNewtonianForce(particle,
                                                                  5,
-                                                                 0.1))
+                                                                 0.001))
                                                                   
                                                                   
         elif self.interaction_type == None:
@@ -100,7 +133,7 @@ class OrbitalSystem(object):
                      -((2*f[0,1]*(self.a(t)*self.M+(-(self.a(t)**2)*self.M+f[0,0]**3)*f[1,1]))/(f[0,0]*(2*(self.a(t)**2)*self.M+(self.a(t)**2)*f[0,0]+(f[0,0]**3))))+rad[2,1]]])
         
         return(farts.G2xy(G,r,phi))
-        
+      
     def UpdateStateVectorRK4(self,t):
         self.event_horizon = self.get_event_horizon(t)
         new_state = np.ndarray((self.Nparticles,2,2))
@@ -110,6 +143,7 @@ class OrbitalSystem(object):
             #sys.stdout.flush()
             kstate = np.copy(self.state)
             #do RK4 shit
+            #is copying the arrays necessary? this might be causing massive slowdowns
             k1 = self.dt * self.SingleParticleDerivativeVector(kstate,particle, t)
             kstate[particle] = np.copy(self.state[particle]) + k1/2
             k2 = self.dt * self.SingleParticleDerivativeVector(kstate,particle,t+(self.dt/2))
@@ -141,52 +175,48 @@ class OrbitalSystem(object):
         
 
         #Cleanup collided particles
-        ## For each collision in the collision_dict,
-        ## grab the particles associated with that collision
-        ## grab the coordinates of the collision
-        ## sum the masses of the particles that are colliding
-        ## sum the momenta
-        ## add all the offending particles to "big_particle_list"
-        ## add the new mass to "big_masses_list"
-        ## delete all the particles and masses in "big_particle_list"
-        ## add the new particle and new mass.
-        
-        if self.collision_dict != {}:
-            for key in self.collision_dict:
-                particles = self.collision_dict[key][1]
-                m = 0
-                x = self.collision_dict[key][0][0]
-                y = self.collision_dict[key][0][1]
-                mvx = 0
-                mvy = 0
-                for particle in particles:
-                    m += self.masses[particle]
-                for particle in particles:
-                    mvx += (self.state[particle][1][0]*self.masses[particle])
-                    mvy += (self.state[particle][1][1]*self.masses[particle])
-                    big_particle_list.append(particle)
-                new_masses_list.append(m)
-            new_particle = [[x,y],[mvx/m,mvy/m]]
-            big_vector_list.append(new_particle)
+        if self.collisions == "inelastic":
+            if self.collision_dict != {}:       
+                for key in self.collision_dict:
+                    particles = self.collision_dict[key][1]
+                    m = 0
+                    x = self.collision_dict[key][0][0]
+                    y = self.collision_dict[key][0][1]
+                    mvx = 0
+                    mvy = 0
+                    for particle in particles:
+                        m += self.masses[particle]
+                    for particle in particles:
+                        mvx += (self.state[particle][1][0]*self.masses[particle])
+                        mvy += (self.state[particle][1][1]*self.masses[particle])
+                        big_particle_list.append(particle)
+                    new_masses_list.append(m)
+                new_particle = [[x,y],[mvx/m,mvy/m]]
+                big_vector_list.append(new_particle)
 
-            self.masses = np.delete(self.masses,big_particle_list,axis=0)
-            self.masses = np.append(self.masses,np.array(new_masses_list),axis = 0)
-            new_state = np.delete(new_state,big_particle_list,axis=0)
-            new_state = np.append(new_state,np.array(big_vector_list),axis = 0)
-            self.Nparticles = len(new_state)
-            self.collision_dict = {}
-
+                self.masses = np.delete(self.masses,big_particle_list,axis=0)
+                self.masses = np.append(self.masses,np.array(new_masses_list),axis = 0)
+                new_state = np.delete(new_state,big_particle_list,axis=0)
+                new_state = np.append(new_state,np.array(big_vector_list),axis = 0)
+                self.Nparticles = len(new_state)
+                self.collision_dict = {}
+            """elif self.collisions == "elastic":
+                if self.collision_dict != {}:
+                    for key in self.collision_dict:
+                        particles = self.collision_dict[key][1]"""
+                        
         return(new_state)
     
     
     def TimeEvolve(self,nsteps,comments,write=True):
         self.cleanup = []
+        self.nsteps = nsteps
         t=0
         
         ##Get init_state
         if self.use_state == None:
             self.cleanup = []
-            self.state = np.copy(farts.make_initial_conditions(self.start_particles))
+            self.state = np.copy(farts.make_initial_conditions2(self.start_particles,self.M,self.a(0)))
             self.init_state = np.copy(self.state)
             self.Nparticles = len(self.state)
         else:
@@ -272,6 +302,7 @@ class OrbitalSystem(object):
                 self.collision_dict[key][1].remove(particle)
         self.Nparticles = len(self.state)
     
+    
     def SingleParticleNewtonianForce(self, i, soi_radius, collision_radius):
         
         forces = np.zeros([self.Nparticles,2])
@@ -289,12 +320,10 @@ class OrbitalSystem(object):
                 y2 = self.state[particle_num,0,1]
                 distance2 = ((x2-x1)**2+(y2-y1)**2)
                 if distance2 < soi_radius:
-                    jforce = m2/distance2
-                    jforcedir = [x2-x1,y2-y1]/np.sqrt(distance2)
-                    forces[particle_num] = jforce*jforcedir
-                    
-                    
-                    
+                    if self.interaction == "ClassicalNBody":
+                        jforce = m2/distance2
+                        jforcedir = [x2-x1,y2-y1]/np.sqrt(distance2)
+                        forces[particle_num] = jforce*jforcedir
                     """
                     if the collision dict is empty, put in the two particles being evaluated.
                     
@@ -303,21 +332,19 @@ class OrbitalSystem(object):
                     if the secondary particle is not in there, put it in the entry with the main particle
                     
                     """
-                if self.collisions == True and i not in self.cleanup and particle_num not in self.cleanup:
-                    if distance2 < collision_radius:
-                        if self.collision_dict == {}:
-                            self.collision_dict[0] = [(x1,y1),[i,particle_num]]
-                        else:
-                            i_val = self.dict_check(self.collision_dict, i)
-                            keynum = max(self.collision_dict)+1
-                            if i_val == -1:
-                                self.collision_dict[keynum] = [(x1,y1),[i]]
-                                i_val = keynum
-                            elif self.dict_check(self.collision_dict, particle_num) == -1:
-                                self.collision_dict[i_val][1].append(particle_num)
-                                
-                                
-            
+                    if self.collisions != False and i not in self.cleanup and particle_num not in self.cleanup:
+                        if distance2 < collision_radius:
+                            if self.collision_dict == {}:
+                                self.collision_dict[0] = [(x1,y1),[i,particle_num]]
+                            else:
+                                i_val = self.dict_check(self.collision_dict, i)
+                                keynum = max(self.collision_dict)+1
+                                if i_val == -1:
+                                    self.collision_dict[keynum] = [(x1,y1),[i]]
+                                    i_val = keynum
+                                elif self.dict_check(self.collision_dict, particle_num) == -1:
+                                    self.collision_dict[i_val][1].append(particle_num)
+					
         return(np.sum(forces,axis=0))
     
     
@@ -389,25 +416,89 @@ class OrbitalSystem(object):
                 what_key = key
         return what_key
     
-    def movie(self,skip_mkdir = False):
+    def get_event_horizon(self,t):
+        return(self.M + np.sqrt(self.M**2 - self.a(t)**2))
+        
+        
+        
+        
+	#####  Plotting Tools  #####
+    
+    def movie(self,type,function = "count"):
+        if type == 'position':
+            self.make_position()
+        elif type == 'heatmap':
+            self.make_heatmap(function)
+        elif type == None:
+            self.make_heatmap()
+            self.make_position()
+    
+    def make_heatmap(self,function):
         print("\n")
-        print("Creating movie...")
-        if skip_mkdir == False:
-            os.mkdir("{}/movie".format(self.dirname))
-            os.mkdir("{}/movie/frames".format(self.dirname))
+        print("Creating animated heatmap...")
         plt.figure()
         n=1
         for fname in self.fname_list:
-            data = fits.open(fname)
+            wholedata = fits.open(fname)
+            if self.skip_mkdir == False:
+                    os.mkdir("{}/movies").format(self.dirname)
+                    os.mkdir("{}/movies/heatmap".format(self.dirname))
+                    os.mkdir("{}/movies/heatmap/frames".format(self.dirname))
+            for i in xrange(1,len(wholedata)):
+            	data = wholedata[i].data
+                bins = scipy.stats.binned_statistic_2d(data['X'],
+                                                       data['Y'],
+                                                       data['MASS'],
+                                                       statistic=function,
+                                                       bins=50,
+                                                       range=[[-30,30],[-30,30]])
+				
+                x,y = np.where(~np.isnan(bins[0]))
+                plt.scatter(x,y,c=bins[0][np.where(~np.isnan(bins[0]))],
+                                                            marker='s',
+                                                            edgecolors='none',
+                                                            s=200)
+                t = n*self.dt
+                plt.xlim(0,48)
+                plt.ylim(0,48)
+                circle1 = plt.Circle((24,24), radius = self.get_event_horizon(t), color = 'r', fill = False)
+                fig = plt.gcf()
+                fig.gca().add_artist(circle1)
+                fig.gca().axes.get_xaxis().set_visible(False)
+                fig.gca().axes.get_yaxis().set_visible(False)
+                plt.axes().set_aspect('equal')
+                plt.title('Nparticles: {}'.format(len(data['X'])))
+                cb = plt.colorbar()
+                plt.clim(0,15)
+                cb.set_label('Number density')
+                plt.scatter(24,24,marker="x", color="white")
+                fname = "{}/movies/heatmap/frames/{}.png".format(self.dirname,n)
+                plt.savefig(fname)
+                plt.clf()
+                sys.stdout.write('\rFrame {} completed'.format(n))
+                sys.stdout.flush()
+                n+=1  
+            wholedata.close()
+        os.system("ffmpeg -framerate 300 -i {}/movies/heatmap/frames/%d.png -c:v libx264 -r 30 -pix_fmt yuv420p {}/movies/heatmap/out.mp4".format(self.dirname,self.dirname))
+
+    def make_position(self):
+        print("\n")
+        print("Creating position space movie...")
+        plt.figure()
+        n=1
+        for fname in self.fname_list:
+            data = fits.open(fname)[1].data
+            if self.skip_mkdir == False:
+                os.mkdir("{}/movies/position_space".format(self.dirname))
+                os.mkdir("{}/movies/position_space/frames".format(self.dirname))
             for i in xrange(1,len(data)):
-                self.plotdata = np.rec.array([data[i].data["X"],data[i].data["Y"],data[i].data["mass"]],names=('x','y','mass'))
-                plt.scatter(self.plotdata['x'],self.plotdata['y'],c=self.plotdata['mass'])
+                plt.scatter(data['X'],data['Y'],c=data['MASS'])
                 cb = plt.colorbar()
                 plt.clim(0,50)
                 cb.set_label('Particle mass')
                 plt.scatter(0,0,marker="x", color="black")
                 t = n*self.dt
-                circle1 = plt.Circle((0,0),radius = self.get_event_horizon(t),color='r',fill=False)
+                circle1 = plt.Circle((0,0), radius = self.get_event_horizon(t), color = 'r', fill = False)
                 fig = plt.gcf()
                 fig.gca().add_artist(circle1)
                 fig.gca().axes.get_xaxis().set_visible(False)
@@ -415,25 +506,50 @@ class OrbitalSystem(object):
                 plt.xlim(-30,30)
                 plt.ylim(-30,30)
                 plt.axes().set_aspect('equal')
-                plt.title('Nparticles: {}'.format(len(self.plotdata['x'])))
-                fname = "{}/movie/frames/{}.png".format(self.dirname,n)
+                plt.title('Nparticles: {}'.format(len(data['X'])))
+                fname = "{}/position_space/frames/{}.png".format(self.dirname,n)
                 plt.savefig(fname)
                 plt.clf()
                 sys.stdout.write('\rFrame {} completed'.format(n))
                 sys.stdout.flush()
                 n+=1
             data.close()
-        
         print('\n')
-        os.system("ffmpeg -framerate 300 -i {}/movie/frames/%d.png -c:v libx264 -r 30 -pix_fmt yuv420p {}/movie/out.mp4".format(self.dirname,self.dirname))
-        
-    def get_event_horizon(self,t):
-        return(self.M + np.sqrt(self.M**2 - self.a(t)**2))
+        os.system("ffmpeg -framerate 300 -i {}/movies/position_space/frames/%d.png -c:v libx264 -r 30 -pix_fmt yuv420p {}/movies/position_space/out.mp4".format(self.dirname,self.dirname))
+
+    def plot_n(self):
+        ns = np.ndarray(len(self.nsteps))
+        i=0
+        for fname in self.fname_list:
+            data = fits.open(fname)
+            ns[i] = len(data[1].data)-1
+            i+=1
+        ns = ns/self.init_particles
+        plt.plot(ns)
+        plt.set_xlabel('Timestep')
+        plt.set_ylabel('\r$n_step/N$')
     
-    def densityplot(self,frame):
-        raise ValueError("This is not implemented yet")
-    
-    def stats(self, interval, statistical_function):
-        #If len(interval)==1: return histogram of that frame
-        #Else: return plot of statistical_function over interval
-        raise ValueError("This is not implemented yet")
+    def plot_rd(self):
+
+    	r_arr = np.ndarray(self.nsteps)
+    	rd_arr = np.ndarray(self.nsteps)
+    	n=1
+    	for fname in self.fname_list:
+    		wholedata = fits.open(fname)
+    		for i in xrange(1,len(wholedata)):
+    			data = wholedata[i].data
+    			r = np.mean(np.sqrt(data['X']**2+data['Y']**2))
+    			r_arr[n] = r
+    			rd = (1/r)*(data['X']*data['Xd']+data['Y']*data['Yd'])
+    			rd_arr[n] = np.mean(rd)
+    			n+=1
+    		print(fname)
+    	r_arr = r_arr/r_arr[0]
+    	plt.plot(r_arr,linewidth=2,color='black')
+    	plt.xlabel(r'Timestep')
+    	plt.ylabel(r'$\frac{<r>}{<r_0>}$',fontsize=16)
+    	plt.show()
+    	plt.plot(rd_arr,linewidth=2,color='black')
+    	plt.xlabel(r'Timestep')
+    	plt.ylabel(r'$<\dot{r}>$',fontsize=16)
+    	plt.show()
